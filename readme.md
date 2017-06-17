975743e3bcc52f73ad6a15a64bd391e35f63920c
mancini85 <fredmancini85@gmail.com>
2017-06-17 11:09:00 +0300

    iniial repo

---
+++ readme.md
@@ -0,0 +1,20 @@
+<p align="center"><img src="https://laravel.com/assets/img/components/logo-laravel.svg"></p>
+
+<p align="center">
+<a href="https://travis-ci.org/laravel/framework"><img src="https://travis-ci.org/laravel/framework.svg" alt="Build Status"></a>
+<a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/d/total.svg" alt="Total Downloads"></a>
+<a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/v/stable.svg" alt="Latest Stable Version"></a>
+<a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/license.svg" alt="License"></a>
+</p>
+
+###################
+What is ecp
+###################
+
+ecp is platform that connects community members,faith-based organizations (FBO),community-based organizations (CBOs),Non-Governmental Organizations (NGOs) and Government Policing Agencies(GPAs)to share information and empower the community at large.
+
+*******************
+Release Version Information
+*******************
+
+Version 1.0.0
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,314 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="connect | share | empower">
+        <meta name="author" content="machini technologies">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>ecp</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Bootstrap -->
+    
+    
+    <!-- Main Style -->
+    <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    
+  
+    <!--[if lt IE 9]>
+        <script src="js/html5shiv.js"></script>
+        <script src="js/respond.min.js"></script>
+    <![endif]-->      
+    </head>
+    <body>
+    <nav class="navbar navbar-default  navbar-fixed-top" role="navigation">
+<!-- Brand and toggle get grouped for better mobile display -->
+<div class="navbar-header">
+       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+             <span class="sr-only">Toggle navigation</span>
+                 <span class="icon-bar"></span>
+                       <span class="icon-bar"></span>
+                 <span class="icon-bar"></span>
+        </button>
+</div> 
+<a class="logo" href="#">
+    <img src="assets/img/ecp.png"  alt="ecp" style="height: 70px; width: 80px;">
+  </a>
+<!-- Collect the nav links, forms, and other content for toggling -->
+       <div class="collapse navbar-collapse" id="cssmenu">
+            <ul class="nav navbar-nav navbar-right">
+                  <li class="scroll"><a href="/">Home</a></li>
+                  <li class="scroll"><a href="about">About</a></li>
+                  <li class="scroll"><a href="services">Services</a></li>
+                  <li class="scroll"><a href="contact">Contact Us</a></li>
+                  <li><a href="/login">Login</a></li>
+                  <li><a href="/register">Register</a></li>   
+            </ul>
+        </div><!-- /.navbar-collapse -->
+</nav>
+<!-- intro -->
+<div class="/">
+        <div id="cd-intro-background">
+           <div class="col-sm-12 col-md-12">
+            <div id="intro">
+                 <div class="centered-words show-for-small-only">
+                     <div class="motto">
+                         <span class="red">connect.</span>
+                         <span class="black">share.</span> 
+                        <span class="green">Empower.</span><br/><br/>
+                     </div>
+                </div> 
+              </div> 
+          </div>
+          <div class="text-center"><a href="index.php/register" class="btn btn-success ">Register Now </a></div>
+      </div>
+  </div>
+
+<!-- end intro -->
+<!-- about ecp -->
+<div id="about">
+<div class="row">
+<div class="content">
+<div class="text-center">
+<h3 class="grey">Welcome to <span class="span">ecp </span></h3>
+<p class="#">ecp is platform that connects community members,faith-based organizations (FBO),community-based organizations (CBOs)<br>,Non-Governmental Organizations (NGOs) and Government Policing Agencies(GPAs)<br>to share information and empower the community at large.
+</p>
+ <!--reasons to use ecp-->
+<h3 class="#">Why use <span class="span">ecp</span></h3>
+ 
+           <div class="col-sm-6 col-md-3">
+            <!-- Service-Block-1 Item Starts -->
+            <div class="service-item wow fadeInUpQuick animated" data-wow-delay=".5s">
+              <div class="icon-wrapper">
+                <i class="icon-layers pulse-shrink">
+                </i>
+              </div>
+              <h2>
+               Efficiency
+              </h2>
+              <p>
+              Its easy and fast.
+              </p>
+            </div>
+            <!-- Service-Block-1 Item Ends -->
+          </div>
+           <div class="col-sm-6 col-md-3">
+            <!-- Service-Block-1 Item Starts -->
+            <div class="service-item wow fadeInUpQuick animated" data-wow-delay=".5s">
+              <div class="icon-wrapper">
+                <i class="icon-layers pulse-shrink">
+                </i>
+              </div>
+              <h2>
+                Reliable
+              </h2>
+              <p>
+                Provide Resourcefull information.
+              </p>
+            </div>
+            <!-- Service-Block-1 Item Ends -->
+          </div> 
+          <div class="col-sm-6 col-md-3">
+            <!-- Service-Block-1 Item Starts -->
+            <div class="service-item wow fadeInUpQuick animated" data-wow-delay=".5s">
+              <div class="icon-wrapper">
+                <i class="icon-layers pulse-shrink">
+                </i>
+              </div>
+              <h2>
+                Alerts
+              </h2>
+              <p>Receive sms and email alerts to confirm <br>membership  status.</p>
+              
+            </div>
+            <!-- Service-Block-1 Item Ends -->
+          </div>
+          <div class="col-sm-6 col-md-3">
+            <!-- Service-Block-1 Item Starts -->
+            <div class="service-item wow fadeInUpQuick animated" data-wow-delay=".5s">
+              <div class="icon-wrapper">
+                <i class="icon-layers pulse-shrink">
+                </i>
+              </div>
+              <h2>
+               Online Services
+              </h2>
+              <p>Fill online application forms, submit then receive feedback on Any device .</p>
+            </div>
+            <!-- Service-Block-1 Item Ends -->
+          </div>
+</div>              
+</div>
+</div>
+</div>
+<!-- contact us-->
+<div id="contact">
+   <div class="footer1">
+   <h3 class="grey">Let's get Intouch</h3>
+            <div class="container">
+                <div class="row">
+                    
+                    <div class="col-md-3 widget">
+                        <h1 class="widget-title">Contact</h1>
+                        <div class="widget-body">
+                            <address>
+                            <i class="fa fa-phone fa-3x sr-contact"></i> : +254 (0) 776 - 557 - 716<br><br>
+                               <i class="fa fa-envelope-o fa-3x sr-contact"></i> : <a href="mailto:#">fredmancini85@gmail.com</a><br>
+                               
+                                
+                            </address>
+                            <div class="contact-address">
+                            <h1 class="widget-title">Address</h1>
+                            <address>
+                            Machini Technologies,<br>
+                            Lakehub - Ground Floor,<br>
+                            Kisumu - Nairobi Road,<br>
+                            P.O Box 12 - 40101,<br>
+                            Ahero - Kenya<br>
+                          <a href="http://www.machinitech.co.ke/"> www.machinitechnologies.co.ke</a>
+                            </address>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 widget">
+                        <h1 class="widget-title">Follow Us</h1>
+                        <div class="widget-body">
+                            <p class="follow-me-icons">
+                                <a href="https://www.facebook.com/"><i class="fa fa-facebook fa-2"></i></a>
+                                <a href="https://twitter.com/"><i class="fa fa-twitter fa-2"></i></a>
+                                <a href="http://instagram.com//"><i class="fa fa-instagram fa-2"></i></a>
+                                <a href="http://googleplus.com//"><i class="fa fa-google-plus fa-2"></i></a>
+                                <!--a href="http://linkedin.com//"><i class="fa fa-linkedin-square fa-2"></i></a-->
+                                <!--a href="http://youtube.com//"><i class="fa fa-youtube fa-2"></i></a-->
+                            </p>    
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 widget">
+                        <h1 class="widget-title">Send a message</h1>
+                        <div class="widget-body">
+                            <div class="status alert alert-success" style="display: none"></div>
+                            <form id="main-contact-form" class="contact-form" name="contact-form" method="post" action="sendemail.php">
+                                <div class="form-group">
+                                    <input type="text" name="name" class="form-control" required="required" 
+                                    value=" Name *" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;">
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" name="email" class="form-control" required="required" value="Email *"
+                                    value=" Name *" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" name="name" class="form-control" required="required" value="Subject *"
+                                    onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;">
+                                </div>
+                                
+                                <div class="form-group">
+                                    <textarea name="message" id="message" required="required" class="form-control" rows="4" placeholder="Type your message here ...." ></textarea >
+                                </div>  
+                                <div class="col-sm-6">
+                               <label class="checkbox"><input type="checkbox" name="check"> Subscribe SMS
+                               </label>
+                               </div>                      
+                                <div class="col-sm-6">
+                                    <button type="submit" class="btn btn-primary pull-right">Send Message
+                                    &nbsp;&nbsp; <i class="fa fa-envelope"></i></button>
+                                </div>
+                            </form>        
+                        </div>
+                        </div>
+                    </div>
+
+                </div> <!-- /row of widgets -->
+            </div>
+            <div class="footer2">
+            <div class="container">
+                <div class="row"><div class="col-md-6 widget">
+                        <div class="widget-body">
+                            <p class="simplenav">
+                                 &copy; <?=date('Y');?>. All Rights Reserved.
+                            </p>
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-6 widget">
+                        <div class="widget-body">
+                            <p class="text-right">
+                                <a href="#">Home</a> |
+                                <a href="#">Help</a> |
+                                <a href="#">Privacy Policy</a> |
+                                <a href="#">Terms of use</a>
+                            </p>
+                        </div>
+                    </div>   
+
+                </div> <!-- /row of widgets -->
+            </div>
+        </div>
+        <!-- Go To Top Link -->
+    <a href="#" class="back-to-top">
+      <i class="fa fa-angle-up ">
+      </i>
+    </a>
+
+    <div class="bottom"> <a href="#" class="settings"></a> </div>
+</div>
+<!-- end contact us -->       
+    </body>
+</html>
